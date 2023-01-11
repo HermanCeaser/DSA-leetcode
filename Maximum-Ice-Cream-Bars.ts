3cdb0d8649c020793d4b3feac631ce70273c595a
Herman Ceaser <ceaserbanks09@gmail.com>
2023-01-11 17:31:42 +0300

    git commit -m "initital Setup"

---
+++ Maximum-Ice-Cream-Bars.ts
@@ -0,0 +1,16 @@
+function maxIceCream(costs: number[], coins: number): number {
+    let maxIceCreamBars = 0
+    let previousSum = 0;
+
+    costs.sort((a, b) => a - b );
+    for(let i=0; i< costs.length; i++){
+        previousSum += costs[i];
+        if (previousSum > coins) break;
+        maxIceCreamBars = i+1;
+    }
+
+    return maxIceCreamBars
+};
+
+// Test Cases
+const maxCreamBars: number = maxIceCream([4, 5, 8, 9], 6)
